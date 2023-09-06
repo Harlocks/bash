1f54d3d7f9966f1643498baea00fc5a5f1120969
Rick <h4rt3ck@pm.me>
2023-09-06 18:53:12 -0300

    chore: Adição das funções básicas de cálculo
    Adicionado soma, subtração, multiplicação e divisão (sem função de resto ainda)

--- scripts/calc.sh
+++ scripts/calc.sh
@@ -16,12 +16,66 @@ num1=$1
 num2=$2
 
 # -----------------------------------
+# Verifica se foi passado argumentos na chamada de comando
 
 if [ -z $num1 -o -z $num2 ]
 then
   echo "❗Argumentos insuficientes"
-  echo "❗Abortando"
+  echo "❗Abortando..."
   exit 1
 fi
 
+# -----------------------------------
+
+echo "-----------------------------------"
+echo "1) - Soma"
+echo "2) - Subtração"
+echo "3) - Multiplicação"
+echo "4) - Divisão"
+echo "q) - Sair"
+echo "------------------------------------"
+read -p "Opção -> " opcao_1
+echo "------------------------------------"
+
+case "$opcao_1" in
+  1) 
+    clear
+    echo "------------------------------------"
+    echo "✅ $num1 + $num2 = $(expr $num1 \+ $num2)"
+    echo "------------------------------------"
+    ;;
+  2)
+    clear
+    echo "------------------------------------"
+    echo "✅ $num1 - $num2 = $(expr $num1 \- $num2)"
+    echo "------------------------------------"
+    ;;
+  3)
+    clear
+    if [ $num1 -eq 0 -o $num2 -eq 0 ]
+    then
+      echo "❗ Algum dos números tem o valor de 0"
+      echo "❗ Abortando..."
+      exit 1
+    else
+      echo "------------------------------------"
+      echo "✅ $num1 X $num2 = $(expr $num1 \* $num2)"
+      echo "------------------------------------"
+    fi
+    ;;
+
+  4)
+    clear
+    if [ $num1 -eq 0 -o $num2 -eq 0 ]
+    then
+      echo "❗ Algum dos números tem o valor de 0"
+      echo "❗ Abortando..."
+      exit 1
+    else
+      echo "------------------------------------"
+      echo "✅ $num1 / $num2 = $(expr $num1 / $num2)"
+      echo "------------------------------------"
+    fi
+    ;;
+esac
 
