3d79689ba10c80d21e7721aa9858a8e8b105a4b0
Rick <h4rt3ck@pm.me>
2023-09-06 18:04:30 -0300

    file: scripts/calc.sh

---
+++ scripts/calc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# -----------------------------------
+# Autor : Erick Robert
+# Email : h4rt3ck@pm.me
+# GitHub: https://github.com/harlocks
+# -----------------------------------
+# Data  : 06/09/2023
+# -----------------------------------
+# Script básico para fazer cálculos
+# -----------------------------------
+clear
+
