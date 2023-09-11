09ceeb7117df97360182789ef8870c08c5fa2601
Rick <h4rt3ck@pm.me>
2023-09-11 18:38:49 -0300

    chore: Script básico pra gerar backup.

---
+++ scripts/generateBackup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# -----------------------------------
+# Autor : Erick Robert
+# Email : h4rt3ck@pm.me
+# GitHub: https://github.com/harlocks
+# -----------------------------------
+# Data  : 11/09/2023
+# -----------------------------------
+# Script básico para gerar backups
+# -----------------------------------
+clear
+
+
+
