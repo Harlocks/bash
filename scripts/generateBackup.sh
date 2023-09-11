8a114e7ae3c3537c13dbb60b6035b552cc7f349f
Rick <h4rt3ck@pm.me>
2023-09-11 19:08:26 -0300

    chore: função de verificação e arquivo de teste
    Adicionado uma função pra verificar se a pasta Backups existe no diretório HOME do seu usuário. Caso não exista, ele vai sair do script com o código de execução 1. Também foi adicionado um script pra fazer testes de funções aleatórias, podendo ser modificado e comentado. Utilizado o método mais compatível com o PADRÃO-POSIX (test).

--- scripts/generateBackup.sh
+++ scripts/generateBackup.sh
@@ -10,5 +10,16 @@
 # -----------------------------------
 clear
 
+# -----------------------------------
+if ! test -d $HOME/Backups
+then
+	echo "❗ Diretório Backups não existe..."
+	echo "❗ Abortando..."
+	exit 1
+fi
+
+# -----------------------------------
+
+echo "TESTE"
 
 
