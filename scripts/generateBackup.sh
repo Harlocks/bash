1a031a40dfe33860ed6807e7e16f6c741783ddc9
Rick <h4rt3ck@pm.me>
2023-09-12 17:43:30 -0300

    feat: Adicionado comando e verificação de pasta
    Logs de erros (stder) e logs padrão (stdout) são redirecionados para arquivos de textos separados. Se não existir uma pasta de backups, o usuário vai ser indagado sobre a criação de um novo diretório, caso ele concorde, será criado o diretório com a nomenclatura padrão ($HOME/Backups). Caso exista um backup criado, o usuário também será perguntado se deseja prosseguir com o processo de backup, caso não, o programa é abortado.

--- scripts/generateBackup.sh
+++ scripts/generateBackup.sh
@@ -14,14 +14,31 @@ clear
 
 data=$(date +%Y%m%d%H%M)
 back_folder="$HOME/Backups"
-week_verify=$(find $back_folder -type f -daystart -ctime 7)
+back_name="backup_home_$data.tgz"
+# back_home=
+week_verify=$(find $back_folder -type f -ctime 7 -name backup_home_\*)
 
 # -----------------------------------
 if ! test -d $HOME/Backups
 then
 	echo "❗ Diretório Backups não existe..."
-	echo "❗ Abortando..."
-	exit 1
+	echo "❗ Gostaria de criar a pasta de Backups?"
+	read -p "[S/N] -> " opcao_create
+	case "$opcao_create" in
+	  S|s)
+	       clear
+	       mkdir $back_folder
+	       echo "✅ Pasta criada com sucesso"
+	       ;;
+	  N|n)
+	       clear
+	       echo "❗ Abortando..."
+	       exit 1
+	       ;;
+	  *)
+	       echo "❗ Opção inválida"
+	esac
+
 fi
 
 # -----------------------------------
@@ -33,7 +50,7 @@ then
   case "$opcao_1" in
   	S|s) 
 	  clear
-	  Prosseguindo...
+	  echo "Prosseguindo..."
 	  ;;
   	N|n)
 	  clear
@@ -41,16 +58,16 @@ then
 	  ;;
 	*)
 	  clear
-	  echo "Opção inválida"
+	  echo "❗ Opção inválida"
 	  ;;
   esac
 else
   echo "⌛ Não foi detectado nenhum backup na última semana."
-  echo "⌛ Continuando com backup...
+  echo "⌛ Continuando com backup..."
 fi
 
 
 # -----------------------------------
 
-
-
+tar -cvzf $back_name . > logs.txt 2> error-logs.txt
+mv $back_name $back_folder
