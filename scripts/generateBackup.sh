099ff901e891b375c4822b586dd36f16d57f3420
Rick <h4rt3ck@pm.me>
2023-09-12 17:17:58 -0300

    feat: Adicionado verificação de backups nos últimos 7 dias
    Se algum backup foi criado na última semana, ele vai perguntar pra o usuário se deseja continuar. Levando em consideração que a pasta de verificação dos backups é $HOME/Backups.

--- scripts/generateBackup.sh
+++ scripts/generateBackup.sh
@@ -11,6 +11,12 @@
 clear
 
 # -----------------------------------
+
+data=$(date +%Y%m%d%H%M)
+back_folder="$HOME/Backups"
+week_verify=$(find $back_folder -type f -daystart -ctime 7)
+
+# -----------------------------------
 if ! test -d $HOME/Backups
 then
 	echo "❗ Diretório Backups não existe..."
@@ -20,6 +26,31 @@ fi
 
 # -----------------------------------
 
-echo "TESTE"
+if test "$week_verify"
+then
+  echo "Foi detectado um backup na última semana"
+  read -p "Gostaria de continuar? [S/N]" opcao_1
+  case "$opcao_1" in
+  	S|s) 
+	  clear
+	  Prosseguindo...
+	  ;;
+  	N|n)
+	  clear
+	  exit 1
+	  ;;
+	*)
+	  clear
+	  echo "Opção inválida"
+	  ;;
+  esac
+else
+  echo "⌛ Não foi detectado nenhum backup na última semana."
+  echo "⌛ Continuando com backup...
+fi
+
+
+# -----------------------------------
+
 
 
