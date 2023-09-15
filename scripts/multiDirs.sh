46f294db42586426a1cea07010caa8b4798a6963
Rick <h4rt3ck@pm.me>
2023-09-15 16:20:05 -0300

    feat: Pasta pomodoro + Arquivo de Script multiDirs.sh
    Adicionado uma pasta de pomodoro com o histórico de pomodoro, usando o padrão 6x30/5x10. Também foi criado um arquivo de script pra gerar múltiplos arquivos, mudando apenas o número de cada um.

---
+++ scripts/multiDirs.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# -----------------------------------
+# Autor : Erick Robert
+# Email : h4rt3ck@pm.me
+# GitHub: https://github.com/harlocks
+# -----------------------------------
+# Data  : 11/09/2023
+# -----------------------------------
+# Script para gerar arquivos
+# -----------------------------------
+clear
+
+
+# -----------------------------------
+
+read -p "Digite o nome do arquivo -> " ARQUIVO
+read -p "Digite o tipo de arquivo[txt, sh]: " TIPO
+read -p "Digite até que quantidade deseja gerar: " QUANTIDADE
+
+for qt in $(seq 1 $QUANTIDADE)
+do
+	touch $ARQUIVO$qt.$TIPO
+done
