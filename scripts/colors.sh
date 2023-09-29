6036cdd2553be99e9352e5cbd5975b46e40b35c6
Rick <h4rt3ck@pm.me>
2023-09-29 13:44:29 -0300

    feat: Adicionado background em colors.sh
    Foi adicionado cores de background no arquivo colors.sh.
    Também foi corrigido as cores e renomeado algumas variáveis.

--- scripts/colors.sh
+++ scripts/colors.sh
@@ -1,32 +1,50 @@
 #!/bin/bash
 # -----------------------------------
-# Autor : Erick Robert
-# Email : h4rt3ck@pm.me
-# GitHub: https://github.com/harlocks
+# @Autor : Erick Robert
+# @Email : h4rt3ck@pm.me
+# @GitHub: https://github.com/harlocks
 # -----------------------------------
-# Data  : 06/09/2023
+# Data   : 06/09/2023
 # -----------------------------------
 # Script básico que armazena todas
 # as cores que o shell suporta.
 # -----------------------------------
 clear
 
-# --------------------------- VALUES -------------------------------
+# -------------------------------------------------------------------
+# --------------------------- @STYLES -------------------------------
+# -------------------------------------------------------------------
 
-c_reset="\e[0m"
+reset="\e[0m" #@ Quando você quiser resetar e deixar as próximas sentenças "Padrão".
+bold="\e[1m" #@ Deixa o texto em negrito.
+underline="\e[4m" #@ Deixa o texto sublinhado.
+blink="\e[5m" #@ Deixa o texto piscando.
+hidden="\e[8m" #@ O texto fica oculto.
 
-c_bold="\e[1m"
-c_underline="\e[4m"
-c_blink="\e[5m"
-c_hidden="\e[8m"
+# -------------------------------------------------------------------
+# --------------------------- @FOREGROUND ---------------------------
+# -------------------------------------------------------------------
 
 f_black="\e[30m"
 f_red="\e[31m"
-f_green="\e[32"
-f_yellow="\e[33"
-f_blue="\e[34"
-f_magenta="\e[35"
-f_cyan="\e[36"
-f_white="\e[37"
+f_green="\e[32m"
+f_yellow="\e[33m"
+f_blue="\e[34m"
+f_magenta="\e[35m"
+f_cyan="\e[36m"
+f_white="\e[37m"
 
-# -teste
+# -------------------------------------------------------------------
+# --------------------------- @BACKGROUND ---------------------------
+# -------------------------------------------------------------------
+
+b_black="\e[40m"
+b_red="\e[41m"
+b_green="\e[42m"
+b_yellow="\e[43m"
+b_blue="\e[44m"
+b_magenta="\e[45m"
+b_cyan="\e[46m"
+b_white="\e[47m"
+
+
