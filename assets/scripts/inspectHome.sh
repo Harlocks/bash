3e50c6ceea15cc3eb5a058032d2b87ec72f05202
ROBERT II <24195153+Harlocks@users.noreply.github.com>
2023-09-30 15:52:31 +0000

    feat: inspectHome.sh

---
+++ assets/scripts/inspectHome.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ----------------------------------------
+# @Author : Erick Robert
+# @Email  : h4rt3ck@pm.me
+# @GitHub : https://github.com/harlocks
+# ----------------------------------------
+# Data    : 30/09/2023
+# ----------------------------------------
+# Script que inspeciona a pasta HOME de todos
+# os usuários da máquina a procura de arquivos
+# com uma extensão desejada.
+# ----------------------------------------
+clear
