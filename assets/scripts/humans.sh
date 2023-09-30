1ee6e6dcfc1b879e9958cd9a7b3599125b17e1c7
ROBERT II <24195153+Harlocks@users.noreply.github.com>
2023-09-30 01:18:04 +0000

    fix: humans.sh Corrigido o script pra extrair informações dos usuários REAIS da máquina.

--- assets/scripts/humans.sh
+++ assets/scripts/humans.sh
@@ -8,18 +8,25 @@
 # -----------------------------------
 # Script que extrai os usuários reais da máquina
 # -----------------------------------
-source /etc/login.defs 2> /dev/null
+clear
 
 # -----------------------------------
 # h_user=$(cat -n /etc/passwd |cut -d":" -f1)
-h_UID=$(cat -n /etc/passwd |cut -d":" -f3)
+h_UID=$(cat /etc/passwd |cut -d":" -f3)
+h_passwd=$(cat /etc/passwd)
 # h_home=$(cat -n /etc/passwd |cut -d":" -f6)
 # h_desc=$(cat -n /etc/passwd |cut -d":" -f5)
 
 for teste in $h_UID
 do
-    if test $teste -le 1000; then
-    echo -e "UID -> $teste"
+    if test $teste -ge 1000 -a $teste -le 60000; then
+    x_user=$(grep $teste /etc/passwd |cut -d":" -f1)
+    x_UID=$(grep $teste /etc/passwd |cut -d":" -f3)
+    x_home=$(grep $teste /etc/passwd |cut -d":" -f6)
+    x_desc=$(grep $teste /etc/passwd |cut -d":" -f5)
+
+    echo -e "USUARIO\t\t UID\t HOME\t\t\t DESCRIÇÃO"
+    echo -e "$x_user\t $x_UID\t $x_home\t $x_desc"
     fi
 done
 
