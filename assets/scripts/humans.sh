e5c1549d9522224fe1c48eb61db5c2982efd62ed
ROBERT II <24195153+Harlocks@users.noreply.github.com>
2023-09-30 01:21:09 +0000

    fix: humans.sh -> Removido linhas de comentários Removido outras linhas desnecessárias.

--- assets/scripts/humans.sh
+++ assets/scripts/humans.sh
@@ -11,12 +11,6 @@
 clear
 
 # -----------------------------------
-# h_user=$(cat -n /etc/passwd |cut -d":" -f1)
-h_UID=$(cat /etc/passwd |cut -d":" -f3)
-h_passwd=$(cat /etc/passwd)
-# h_home=$(cat -n /etc/passwd |cut -d":" -f6)
-# h_desc=$(cat -n /etc/passwd |cut -d":" -f5)
-
 for teste in $h_UID
 do
     if test $teste -ge 1000 -a $teste -le 60000; then
@@ -29,6 +23,3 @@ do
     echo -e "$x_user\t $x_UID\t $x_home\t $x_desc"
     fi
 done
-
-# echo -e "USUÁRIO\t UID \t HOME \t DESCRIÇÃO"
-# echo -e "$h_user\t $h_UID\t $h_home\t $h_desc"
