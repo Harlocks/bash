babb939bedcfcc4e7de891431b5f3cd206377d4e
ROBERT II <24195153+Harlocks@users.noreply.github.com>
2023-10-09 20:27:38 +0000

    refactor: Double quotes for preventing globbing and word splitting

--- assets/scripts/humans.sh
+++ assets/scripts/humans.sh
@@ -12,16 +12,16 @@ clear
 
 # -----------------------------------
 
-h_UID=$(cat /etc/passwd |cut -d":" -f3)
+h_UID=$( cut -d":" -f3 /etc/passwd )
 
 for teste in $h_UID
 do
-    if test $teste -ge 1000 -a $teste -le 60000; then
+    if test "$teste" -ge 1000 -a "$teste" -le 60000; then
 
-    x_user=$(grep $teste /etc/passwd |cut -d":" -f1)
-    x_UID=$(grep $teste /etc/passwd |cut -d":" -f3)
-    x_home=$(grep $teste /etc/passwd |cut -d":" -f6)
-    x_desc=$(grep $teste /etc/passwd |cut -d":" -f5)
+    x_user=$(grep "$teste" /etc/passwd |cut -d":" -f1)
+    x_UID=$(grep "$teste" /etc/passwd |cut -d":" -f3)
+    x_home=$(grep "$teste" /etc/passwd |cut -d":" -f6)
+    x_desc=$(grep "$teste" /etc/passwd |cut -d":" -f5)
 
     echo -e "USUARIO\t\t UID\t HOME\t\t\t DESCRIÇÃO"
     echo -e "$x_user\t $x_UID\t $x_home\t $x_desc"
--- assets/scripts/inspectHome.sh
+++ assets/scripts/inspectHome.sh
@@ -12,11 +12,40 @@
 # ----------------------------------------
 clear
 
-busca=$( find /home/* -name '*.mp3' -o -name '*.mp4' -o -name '*.jpg' -o -name '*.png')
-mp3=$( $busca |grep "*.*.mp3" |wc -l )
+if test $# -le 0; then
+    echo "----------------------------------------"
+    echo "Quantidade de parâmetros insuficientes"
+    echo "Usage: $0 [<Directory>]"
+    echo "----------------------------------------"
+    exit 1
+fi
 
-echo "-----------------------------"
-for arquivos in $busca; do
-    echo "Arquivos .MP3: $mp3"
+HOME=$1
+
+if ! test -d "$HOME"; then
+    echo "Usuário ou diretório não existe!"
+    exit 1
+fi
+
+jpg=$( find "$HOME" -name "*.jpg" |wc -l )
+mp3=$( find "$HOME" -name "*.mp3" |wc -l ) 
+mp4=$( find "$HOME" -name "*.mp4" |wc -l )
+
+echo "----------------------------------------"
+echo "Diretório: $HOME"
+for teste in $jpg
+do
+    echo "Arquivos JPG: $teste"
 done
+
+for teste1 in $mp3
+do
+    echo "Arquivos MP3: $teste1"
+done
+
+for teste2 in $mp4
+do
+    echo "Arquivos MP4: $teste2"
+done
+echo "----------------------------------------"
 
