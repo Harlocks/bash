12ea82f4de52459c5f7e0bd482164e748f2e021b
ROBERT II <24195153+Harlocks@users.noreply.github.com>
2023-10-30 12:34:04 +0000

    fix: humans.sh
    Retirado o "título" de dentro do escopo.

--- assets/scripts/humans.sh
+++ assets/scripts/humans.sh
@@ -13,17 +13,23 @@ clear
 # -----------------------------------
 
 h_UID=$( cut -d":" -f3 /etc/passwd )
+OLDIFS=$IFS
+
+IFS=$'\n'
+
+echo -e "USUARIO\t\t UID\t\t HOME\t\t DESCRIÇÃO"
 
 for teste in $h_UID
 do
     if test "$teste" -ge 1000 -a "$teste" -le 60000; then
-
+ 
     x_user=$(grep "$teste" /etc/passwd |cut -d":" -f1)
     x_UID=$(grep "$teste" /etc/passwd |cut -d":" -f3)
     x_home=$(grep "$teste" /etc/passwd |cut -d":" -f6)
     x_desc=$(grep "$teste" /etc/passwd |cut -d":" -f5)
 
-    echo -e "USUARIO\t\t UID\t HOME\t\t\t DESCRIÇÃO"
-    echo -e "$x_user\t $x_UID\t $x_home\t $x_desc"
+    echo -e "$x_user\t\t$x_UID\t\t$x_home\t\t$x_desc"
     fi
 done
+
+IFS=$OLDIFS
